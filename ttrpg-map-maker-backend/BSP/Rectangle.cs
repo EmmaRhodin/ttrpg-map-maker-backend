07737db8bff9bf2fd238732673c8ac829c65c171
EmmaRhodin <143386528+EmmaRhodin@users.noreply.github.com>
2025-02-27 10:41:32 +0100

    Base BSP Algorithm imported from demo

---
+++ ttrpg-map-maker-backend/BSP/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿public struct Rectangle
+{
+    public float X, Y;
+    public float Width, Height;
+
+    public Rectangle(float x, float y, float width, float height)
+    {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
+    public bool IsLargeEnough(float minWidth, float minHeight)
+    {
+        return Width >= minWidth && Height >= minHeight;
+    }
+}
---
+++ ttrpg-map-maker-backend/Controllers/AlgorithmsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ttrpg_map_maker_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
